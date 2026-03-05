ba54235eb25df367eba9021ce4ce9cf94e49ab60
Undead Queen <superville2600@gmail.com>
2026-03-04 18:06:39 -0600

    changed link to github repo

--- vite-project/projects.js
+++ vite-project/projects.js
@@ -2,7 +2,7 @@ export default [
     {
         Name: "Guess the Ghost",
         desc: "Guess the Ghost is a game a twitch streamer can play with their chat. The person who correctly guesses the ghost room and/or type wins a prize within the streamer's discretion.",
-        Link: "https://github.com/QueenofUndead/guesstheghost"
+        Link: "https://github.com/UndeadQueen/guesstheghost"
     },
     {
         Name: "Coming soon",
